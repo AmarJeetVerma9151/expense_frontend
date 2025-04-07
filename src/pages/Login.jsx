989f5f55b5c0f57c095c8c9bf08b614417760ebc
AmarJeetVerma9151 <vermaamarjeet095@gmail.com>
2025-04-08 01:22:10 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = (props) => {
        
 
 <form className="max-w-sm mx-auto w-[500px]">
- 
+<h1 className='text-3xl text-purple-900  mb-4' >Login Page</h1>
   <div className="mb-5">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
     <input ref={emailRef} type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -45,8 +45,8 @@ const Signup = () => {
   return (
     <div className='h-[80vh] flex items-center'>
        
-
 <form className="max-w-sm mx-auto w-[500px]">
+<h1 className='text-3xl text-rose-600 mb-4' >Signup Pages</h1>
   <div className="mb-5">
     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
     <input ref={nameRef} type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="enter your name" required />
